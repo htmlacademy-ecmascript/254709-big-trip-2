2e90f94fab237ad759fb5e65e2f6a911503611ae
Влад <v1ad@inbox.ru>
2025-02-18 21:50:36 +0300

    - Добавит новое разбиение на презенторы

--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
-// import { render, RenderPosition } from './render.js';
+import BigTripPresenter from './presenter/big-trip-presenter.js';
 import WaypointsModel from './model/waypoints-model.js';
 import OffersModel from './model/offers-model.js';
 import DestinationsModel from './model/destinations-model.js';
 import FiltersModel from './model/filters-model.js';
 
-import HeaderPresenter from './presenter/header-presenter.js';
-import WaypointPresenter from './presenter/waypoint-presenter.js';
-
-const tripInfoContainer = document.querySelector('.trip-main');
-const filtersListContainer = tripInfoContainer.querySelector('.trip-controls__filters');
-const listContainer = document.querySelector('.trip-events');
+const tripMainContainer = document.querySelector('.trip-main');
+const tripEventsContainer = document.querySelector('.trip-events');
 
 const waypointsModel = new WaypointsModel();
 const offersModel = new OffersModel();
 const destinationsModel = new DestinationsModel();
 const filtersModel = new FiltersModel();
 
-const headerPresenter = new HeaderPresenter({ tripInfoContainer, filtersListContainer, filtersModel, listContainer });
-const waypointPresenter = new WaypointPresenter({ listContainer: listContainer, waypointsModel, offersModel, destinationsModel });
-headerPresenter.init();
-waypointPresenter.init();
+const bigTripPresenter = new BigTripPresenter({
+  tripMainContainer,
+  tripEventsContainer,
+  waypointsModel,
+  offersModel,
+  destinationsModel,
+  filtersModel
+});
+
+bigTripPresenter.init();
--- src/presenter/big-trip-presenter.js
+++ src/presenter/big-trip-presenter.js
@@ -1,4 +1,79 @@
-import { render, replace } from '../framework/render.js';
-import SortListView from '../view/sort-list-view/sort-list-view.js';
-import WaypointEmptyView from '../view/waypoint-empty-view/waypoint-empty-view.js';
-import EditFormView from '../view/edit-form-view/edit-form-view.js';
+import HeaderPresenter from './header-presenter.js';
+import WaypointPresenter from './waypoint-presenter.js';
+
+export default class BigTripPresenter {
+  #tripMainContainer = null;
+  #tripEventsContainer = null;
+
+  #waypointsModel = null;
+  #offersModel = null;
+  #destinationsModel = null;
+  #filtersModel = null;
+
+  #headerPresenter = null;
+  #waypointPresenters = new Map();
+  #waypoints = [];
+
+  constructor({
+    tripMainContainer,
+    tripEventsContainer,
+    waypointsModel,
+    offersModel,
+    destinationsModel,
+    filtersModel
+  }) {
+    this.#tripMainContainer = tripMainContainer;
+    this.#tripEventsContainer = tripEventsContainer;
+
+    this.#waypointsModel = waypointsModel;
+    this.#offersModel = offersModel;
+    this.#destinationsModel = destinationsModel;
+    this.#filtersModel = filtersModel;
+  }
+
+  init() {
+    this.#waypoints = [...this.#waypointsModel.waypoints];
+    this.#runApp();
+  }
+
+  #runApp() {
+    this.#initHeaderPresenter();
+    this.#initWaypoints();
+  }
+
+  #initHeaderPresenter() {
+    const filtersListContainer = this.#tripMainContainer.querySelector('.trip-controls__filters');
+
+    this.#headerPresenter = new HeaderPresenter({
+      tripInfoContainer: this.#tripMainContainer,
+      filtersListContainer: filtersListContainer,
+      filtersModel: this.#filtersModel,
+      listContainer: this.#tripEventsContainer
+    });
+
+    this.#headerPresenter.init();
+  }
+
+  #initWaypoints() {
+    this.#waypoints.forEach((waypoint) => {
+      this.#renderWaypoint(waypoint);
+    });
+  }
+
+  #renderWaypoint(waypoint) {
+    const waypointPresenter = new WaypointPresenter({
+      listContainer: this.#tripEventsContainer,
+      waypointsModel: this.#waypointsModel,
+      offersModel: this.#offersModel,
+      destinationsModel: this.#destinationsModel
+    });
+
+    waypointPresenter.init(waypoint);
+    this.#waypointPresenters.set(waypoint.id, waypointPresenter);
+  }
+
+  destroy() {
+    this.#waypointPresenters.forEach((presenter) => presenter.destroy());
+    this.#waypointPresenters.clear();
+  }
+}
