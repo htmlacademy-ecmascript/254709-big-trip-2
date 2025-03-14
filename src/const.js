472ab49c988b79a4df51cfe3cce705cd4ea6de02
Влад <v1ad@inbox.ru>
2025-03-14 09:27:36 +0300

    - Дополнит предыдущий коммит правками

--- src/const.js
+++ src/const.js
@@ -1,21 +1,5 @@
 export const POINT_TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
 
-export const CITIES = ['Amsterdam', 'Chamonix', 'Geneva', 'Paris', 'Berlin', 'Prague', 'Tokyo', 'Moscow', 'Saint-Petersburg'];
-
-export const DESCRIPTION = [
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  'Cras aliquet varius magna, non porta ligula feugiat eget.',
-  'Fusce tristique felis at fermentum pharetra.',
-  'Aliquam id orci ut lectus varius viverra.',
-  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.',
-  'Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.',
-  'Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
-  'Sed sed nisi sed augue convallis suscipit in sed felis.',
-  'Aliquam erat volutpat.',
-  'Nunc fermentum tortor ac porta dapibus.',
-  'In rutrum ac purus sit amet tempus.'
-];
-
 export const SortType = {
   DAY: {
     NAME: 'DAY',
@@ -43,8 +27,6 @@ export const Mode = {
   VIEW: 'VIEW',
   EDITING: 'EDITING',
 };
-
-export const PHOTO = 'https://loremflickr.com/248/152?random=';
 
 export const UserAction = {
   UPDATE_WAYPOINT: 'UPDATE_WAYPOINT',
--- src/utils/common.js
+++
@@ -1,7 +0,0 @@
-const getRandomArrayElement = (items) => items[Math.floor(Math.random() * items.length)];
-
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-const getRandomBoolean = () => Math.random() < 0.5;
-
-export { getRandomArrayElement, getRandomInt, getRandomBoolean };
